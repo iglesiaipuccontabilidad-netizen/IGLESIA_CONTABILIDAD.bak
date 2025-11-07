aaa784aa9401b4916f4b07b44c4023263c881621
iglesiaipuccontabilidad-netizen <iglesiaipuccontabilidad@gmail.com>
2025-11-06 23:04:12 -0500

    feat: Implementación del formulario de edición de miembros y corrección de dependencias

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { redirect } from 'next/navigation'
 
-export default async function Home() {
+export default function Home() {
   redirect('/login')
-  return null // This line will never execute
 }
